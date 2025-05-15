a00809da0cc0af6eeec7368f2d7aabafc2289015
jay <abolfazls4yy4h@gmail.com>
2025-05-16 00:47:08 +0430

    [MIGRATE] first migration

--- onlineshop_api/urls.py
+++ onlineshop_api/urls.py
@@ -21,7 +21,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
 
     # swagger routes
     path('api-auth/', include('rest_framework.urls')),
